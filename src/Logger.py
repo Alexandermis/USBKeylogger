eb66e33f6f6427644996ac1be7a9a8abb4b0bec8
Alexander <alexandermis@web.de>
2023-04-16 12:23:53 +0200

    types fix

--- src/Logger.py
+++ src/Logger.py
@@ -4,20 +4,20 @@ import usb.core, usb.util
 class Logger:
     __slots__ = ['vendor_ids']
 
-    def __init__(self):
-        self.vendor_ids: list[str] = self.get_vendor_id()
+    def __init__(self) -> None:
+        self.vendor_ids: list[int] = self.get_vendor_id()
 
     def __del__(self):
         pass
 
     @staticmethod
-    def get_vendor_id() -> list[str]:
+    def get_vendor_id() -> list[int]:
         devices = usb.core.find(find_all=True)
-        vendor_ids: list[str] = []
+        vendor_ids: list[int] = []
         # Enumerate over all USB devices
         for device in devices:
             try:
-                vendor_ids.append(str(device.idVendor))
+                vendor_ids.append(device.idVendor)
             except usb.core.USBError as e:
                 print(f"Error reading Vendor ID: {e}")
         return vendor_ids
@@ -25,8 +25,8 @@ class Logger:
     # TODO: Rewrite this code
     def run(self):
         # Geräteinformationen
-        vendor_id = 0x1234  # Beispiel-Vendor-ID, bitte durch die richtige ID ersetzen
-        product_id = 0x5678  # Beispiel-Product-ID, bitte durch die richtige ID ersetzen
+        vendor_id: int = 0x1234  # Beispiel-Vendor-ID, bitte durch die richtige ID ersetzen
+        product_id: int = 0x5678  # Beispiel-Product-ID, bitte durch die richtige ID ersetzen
 
         # USB-Gerät finden
         dev = usb.core.find(idVendor=vendor_id, idProduct=product_id)
