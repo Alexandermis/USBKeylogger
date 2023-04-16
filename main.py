3d99e5aa27b2b39b3cc49827f103236e8da55bce
Alexander <alexandermis@web.de>
2023-04-16 11:40:26 +0200

    main + DebugLogger

---
+++ main.py
@@ -0,0 +1,9 @@
+import logging
+
+from src.Setup import Setup
+def main():
+    setup = Setup()
+    logging.info("Starting")
+
+if __name__ == "__main__":
+    main()
---
+++ src/Setup.py
@@ -0,0 +1,22 @@
+import sys
+import logging
+
+
+class Logger:
+    __slots__ = ['__root']
+
+    def __init__(self):
+        self.__root = logging.getLogger()
+        self.__root.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s:%(levelname)s : %(message)s')
+        handler.setFormatter(formatter)
+        self.__root.addHandler(handler)
+
+
+class Setup:
+    __slots__ = ['logger']
+
+    def __init__(self):
+        self.logger = Logger()
