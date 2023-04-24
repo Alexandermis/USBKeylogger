675829177ff3fbdb09c7fedf5d93266dc316a7c6
Alexander <alexandermis@web.de>
2023-04-24 18:34:08 +0200

    Added feature args and custom debug logging with args

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import logging
+import argparse
+
 
 from src.Setup import Setup
 from src.Logger import Logger
 
-def main() -> None:
-    setup = Setup()
+def main(args: argparse.ArgumentParser) -> None:
+    setup: Setup = Setup(args)
     logging.info("Starting")
 
 
 if __name__ == "__main__":
-    main()
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(description='Python Hardware Keylogger')
+    parser.add_argument('-m', '--mode', type=str, help='This Logger level d=debug, p=production')
+    args = parser.parse_args()
+    main(args)
--- src/Setup.py
+++ src/Setup.py
@@ -1,22 +1,27 @@
 import sys
 import logging
+import argparse
 
 
 class Logger:
-    __slots__ = ['__root']
+    __slots__ = ['__main_logger', '__mode']
 
-    def __init__(self) -> None:
-        self.__root = logging.getLogger()
-        self.__root.setLevel(logging.DEBUG)
+    def __init__(self, mode: str | None) -> None:
+        self.__mode = mode
+        self.__main_logger = logging.getLogger()
+        if mode == "debug" or mode == "d":
+            self.__main_logger.setLevel(logging.DEBUG)
+        elif mode is None or mode == "production" or mode == "p":
+            self.__main_logger.setLevel(logging.INFO)
         handler = logging.StreamHandler(sys.stdout)
         handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s:%(levelname)s : %(message)s')
         handler.setFormatter(formatter)
-        self.__root.addHandler(handler)
+        self.__main_logger.addHandler(handler)
 
 
 class Setup:
     __slots__ = ['logger']
 
-    def __init__(self):
-        self.logger = Logger()
+    def __init__(self, args):
+        self.logger = Logger((lambda m: m if m else None)(args.mode))
