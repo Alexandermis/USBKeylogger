c90bada1def837055d590d6f7f3da864aa25e6c9
Alexander <alexandermis@web.de>
2023-05-09 17:49:35 +0200

    FileHandler

--- src/KeyLogger.py
+++ src/KeyLogger.py
@@ -35,9 +35,41 @@ class KeyLogger:
             # raise NoDeviceFound(self.__devices)
             pass
         else:
-            device: Optional[usb.core.Device] = usb.core.find(
-                idVendor=id_vendor, idProduct=device_id
-            )
+            try:
+                # Find the USB device
+                dev = usb.core.find(idVendor=id_vendor, idProduct=device_id)
+
+                # Detach the kernel driver if it's already attached
+                if dev.is_kernel_driver_active(0):
+                    try:
+                        dev.detach_kernel_driver(0)
+                    except usb.core.USBError as e:
+                        print("Could not detach kernel driver: %s" % str(e))
+
+                # Release the device from any other processes
+                usb.util.dispose_resources(dev)
+
+                # Claim the device interface
+                usb.util.claim_interface(dev, 0)
+
+                # Do something with the device here...
+
+                # Release the device interface
+                usb.util.release_interface(dev, 0)
+
+                # Reattach the kernel driver if we detached it earlier
+                if dev_was_kernel_driver_active:
+                    dev.attach_kernel_driver(0)
+
+            except usb.core.NoBackendError:
+                print("No backend available")
+            except usb.core.NotFoundError:
+                print("Device not found")
+            except usb.core.USBError as e:
+                print("USB error: %s" % str(e))
+#            device: Optional[usb.core.Device] = usb.core.find(
+#                idVendor=id_vendor, idProduct=device_id
+#            )
 
             return device
 
