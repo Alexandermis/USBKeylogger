866d0144ff1b7ded2deff3535e893b93828adb32
Alexander <alexandermis@web.de>
2023-05-21 17:49:18 +0200

    removed imports

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ if __name__ == "__main__":
     parser.add_argument(
         "-m", "--mode", type=str, help="This Logger level d=debug, p=production"
     )
-    parser.add_argument("-i", "--ip", type=str, help="The server IP Addr.")
-    parser.add_argument("-p", "--port", type=str, help="The server port")
-    parser.add_argument("-d", "--deviceId", type=str, help="The device id")
-    parser.add_argument("-v", "--vendorId", type=str, help="The vendor id")
+    parser.add_argument("-i", "--ip", type=int, help="The server IP Addr.")
+    parser.add_argument("-p", "--port", type=int, help="The server port")
+    parser.add_argument("-d", "--deviceId", type=int, help="The device id")
+    parser.add_argument("-v", "--vendorId", type=int, help="The vendor id")
     parser.add_argument(
         "-k",
         "--keyboard",
